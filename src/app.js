85020b3bdc341e549bd62abfa79602dd9d72cfad
Samantha Ming <samanthaming.ming@gmail.com>
2016-10-12 00:07:49 -0700

    setup redux

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
+import reducers from './reducers';
 import { Header } from './components/common';
 import UserCard from './components/UserCard';
 
 const GitPractice = () => {
   return (
-    <View style={{flex: 1}}>
-      <Header headerText="Git Practice" />
-      <UserCard />
-    </View>
+    <Provider store={createStore(reducers)}>
+      <View style={{flex: 1}}>
+        <Header headerText="Git Practice" />
+        <UserCard />
+      </View>
+    </Provider>
   );
 };
 
